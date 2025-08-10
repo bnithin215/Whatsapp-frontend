6021cf30af978720d32e93a47a3d0b1f98eb482a
bnithin215 <nithinbuggargani@gmail.com>
2025-08-11 00:00:56 +0530

    final commit

--- src/api.js
+++ src/api.js
@@ -1,3 +1,8 @@
+// api.js
 import axios from 'axios';
-const API = axios.create({ baseURL: process.env.REACT_APP_API_URL });
+
+const API = axios.create({
+  baseURL: 'https://whatsapp-backend-rhrf.onrender.com',
+});
+
 export default API;
