d1b300f423b803437a44dc83408743b4e43c1297
bnithin215 <nithinbuggargani@gmail.com>
2025-08-11 17:54:33 +0530

    updated api

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://whatsapp-backend-rhrf.onrender.com',
+  baseURL: 'https://whatsapp-backend-rhrf.onrender.com/api',
 });
 
 export default API;
